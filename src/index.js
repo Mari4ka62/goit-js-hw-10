fff1dbaaef9d551f3954cbf684afc3d50ab5c8cc
Maria Vovchuk <mari4ka62@gmail.com>
2023-03-25 16:01:50 +0200

    Правка2

--- src/index.js
+++ src/index.js
@@ -1,63 +1,61 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
 
 const searchInput = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInformation = document.querySelector('.country-info');
-const DEBOUNCE_DELAY = 700;
+const DEBOUNCE_DELAY = 300;
 
 
 searchInput.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
 
-
 function onInputSearch(e) {
-    const searchValue = searchInput.value.trim();
-    if (searchValue) {
-        fetchCountries(searchValue).then(response => {
-        clearInput(); 
-        chekInfo(response);
-    }).catch(() => {
-        alert('Oops, there is no country with that name');
-        clearInput();
-    });
+    const searchValue = e.target.value.trim();
+    if (!searchValue) {
+       return clearInfo();
     }
-    
-}
-function renderCounrtyList(countries) {
-    return countries.map(({ name, flags })=> {
-        return `<li class='country-list_item'>
-        <img class='country-list_flag' src='${flags.svg}' alt='${name.official}'
-        <h2 class='country-list_name'>${name.official}</h2></li>`
-    }).join('')
+    fetchCountries(searchValue)
+        .then(data=>checkInfo(data))
+        .catch(() => Notify.failure("Oops, there is no country with that name"))
 }
 
-function renderCountriesInfo(counrties){
-  return counrties.map(({capital, population, languages})=>{
-return ` 
-<div class='country-list-info__item'>Capital: ${capital}</div>
-<div class='country-list-info__item'>Population: ${population}</div>
-<div class='country-list-info__item'>Languages: ${Object.values(languages).join(', ')}</div>`
-  }).join('')
-}
-
-function chekInfo(array) {
-    if (array.length>10) {
-        alert ("Too many matches found. Please enter a more specific name.")
+function checkInfo(countries) {
+    if (countries.length>10) {
+       return Notify.info("Too many matches found. Please enter a more specific name.")
     }
-     else if (array.length>2 && array.length<=10) {
-        countryList.insertAdjacentElement('beforeend', renderCounrtyList(array))
+    else if (countries.length >= 2 && countries.length <= 10) {
+        clearInfo();
+        countryList.insertAdjacentHTML('beforeend', renderCounrtyList(countries))
     }
-    else if (array.length===1) {
-        countryList.insertAdjacentElement('beforeend', renderCounrtyList(array));
-        countryInformation.insertAdjacentElement('beforeend', renderCountriesInfo(array));
+    else if (countries.length === 1) {
+        clearInfo();
+        countryInformation.insertAdjacentHTML('beforeend',renderCountryInfo(countries))
+        
     }
 }
+function renderCounrtyList(counrties) {
+    return counrties.map(({ flags, name }) => {
+        return `<li class="country-list_item"><img class="country-list_img" src="${flags.svg}"alt="${name}" width='30' height='20'>${name.official}</li>`
+    }).join('');
 
-function clearInput() {
+
+}
+function renderCountryInfo(countries) {
+    return countries.map(({ name, capital, population, flags, languages }) => {
+        return `<img class="country-info_img" src="${flags.svg}" alt="${name}" width='40' height='30'>
+                <span class="coutry-info_title">${name.official}</span>
+                <p class="country-info_item"><span class='country-info_text'>Capital:</span> ${capital}</p>
+                <p class="country-info_item"><span class='country-info_text'>Population:</span> ${population}</p>
+                <p class="country-info_item"><span class='country-info_text'>Languages:</span> ${Object.values(languages)}</p>`
+    }).join('');
+}
+
+function clearInfo() {
     countryList.innerHTML = '';
     countryInformation.innerHTML = '';
 }
 
 
-
+ 
